ea0001a51defef66f0a38938c319ebb22d115768
Islombek <131392096+Code-Leader@users.noreply.github.com>
2023-04-27 16:29:31 +0500

    Update console.js

--- console.js
+++ console.js
@@ -1,7 +1,10 @@
 // hey this is a commit
-consle.log('Hello World')
-consle.log('Hello World') // 2nd time!
+console.log('Hello World')
+console.log('Hello World') // 2nd time!
 
 function main() {
 return null; 
 }
+main()
+
+// consule.log(main)
