bc5b1cb22811e6f44afee3276b55677c58bdca85
Leader-Islombek <iviuzbek@gmail.com>
2023-05-23 21:18:34 +0500

    Auther Code

--- users/models.py
+++ users/models.py
@@ -1,5 +1,7 @@
-from datetime import datetime, timedelta
 import random
+import uuid
+from rest_framework_simplejwt.tokens import RefreshToken
+from datetime import datetime, timedelta
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from shared.models import BaseModel
@@ -53,6 +55,46 @@ class User(AbstractUser, BaseModel):
         )
         return code
     
+    def check_username(self):
+        if not self.username:
+            temp_username = f'instagram-{uuid.uuid4().__str__().split("-")[-1]}'
+            while User.objects.filter(username=temp_username):
+                temp_username = f"{temp_username}{random.randint(0,9)}"
+            self.username = temp_username
+    
+        def check_email(self):
+            if self.email:
+                normalized_email = self.email.lower()
+                self.email = normalized_email
+
+    def check_pass(self):
+        if not self.password:
+            temp_password = f"password-{uuid.uuid4().__str__().split('-')[-1]}"
+            self.password = temp_password
+
+    def hashing_password(self):
+        if not self.password.startswith('pbkdf2_sha256'):
+            self.set_password(self.password)
+
+    def token(self):
+        refresh = RefreshToken.for_user(self)
+        return {
+            "access": str(refresh.access_token),
+            "refresh_token": str(refresh)
+        } 
+    
+    def clean(self):
+        self.check_email()
+        self.check_username()
+        self.check_pass()
+        self.check_password()
+
+    def save(self, *args, **kwargs):
+        if not self.pk:
+            self.clean()
+        super(User, self).save(*args, **kwargs)
+
+
 
 
 PHONE_EXPIRE = 2
